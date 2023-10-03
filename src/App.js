f78f1a01ff4bb4b5f9ac3924393b8b88c276455a
alainhortiz <alainhortiz@gmail.com>
2023-10-03 18:02:06 -0400

    Definir los componentes de la página

--- src/App.js
+++ src/App.js
@@ -3,22 +3,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <select>
+          <option value="fire">Casa en llamas</option>
+          <option value="futurama">Futurama</option>
+          <option value="history">History Chanel</option>
+          <option value="matrix">Matrix</option>
+          <option value="philosoraptor">Philosoraptor</option>
+          <option value="smart">Smart Guy</option>
+        </select><br/>
+
+        <input type="text" placeholder='linea 1' /><br/>
+        <input type="text" placeholder='linea 2'/><br/>
+        <button>Exportar</button>
+
+        <div className='meme' id='meme'>
+          <span></span><br/>
+          <span></span>
+          <img src='' />
+        </div>
+      </div>
   );
 }
 
